ee2bd5107b7d7d779061e287109f259aa75582a3
Milo Le Joronen <milo.joronen@outlook.com>
2023-08-31 23:41:32 +1000

    Delete phone button worked

--- api/controllers/stats.controller.js
+++ api/controllers/stats.controller.js
@@ -4,6 +4,18 @@ const Contacts = db.contacts;
 const Op = db.Sequelize.Op;
 
 // Calculate stats
-exports.calculate = (req, res) => {
+exports.calculate = async (req, res) => {
+    const phone = await Phones.count();
+    const contact = await Contacts.count();
+    const newstContact = await Contacts.max('createdAt');
+    const oldestContact = await Contacts.min('createdAt');
+
+    const stats = {
+        ContactNum: contact,
+        PhoneNum: phone,
+        newContact: newstContact,
+        oldContact: oldestContact
+    };
     
+    res.send(stats);
 };
